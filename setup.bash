cde442dac11e6d050605f2ad77111740ca6ae931
Niels de Boer <nielsvdeboer@gmail.com>
2020-10-27 12:20:41 +0100

    Create setup script

---
+++ setup.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd ..
+
+# Clone the Universal Robots Driver
+git clone https://github.com/UniversalRobots/Universal_Robots_ROS_Driver.git
+
+# Clone the robotiq descriptions
+git clone -b kinetic-devel https://github.com/TripleSBinPicking/robotiq.git
+
+# Clone the descriptions for the UR robots
+git clone -b calibration_devel https://github.com/fmauch/universal_robot.git
+
+# Install dependencies
+sudo apt update -qq
+rosdep update
+rodep install --from-paths src --ignore-src -y
+
+# Build the workspace
+catkin_make
+
+source devel/setup.bash
+
+cd src
